8c7fae936f044002dd75d97ccd235de5b2476833
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2018-03-28 23:10:23 +0900

    add code

---
+++ README.md
@@ -0,0 +1,24 @@
+[![Travis](https://api.travis-ci.org/kmaehashi/cuda-docker-generator.svg?branch=master)](https://travis-ci.org/kmaehashi/cuda-docker-generator)
+
+# cuda-docker-generator
+
+`cuda-docker-generator` is a tool to generate `Dockerfile` that installs CUDA / cuDNN on arbitrary base Docker image.
+
+Installation steps are automatically downloaded from assets in [nvidia/cuda repository](https://gitlab.com/nvidia/cuda/).
+Images can be run using [nvidia-docker](https://github.com/NVIDIA/nvidia-docker).
+
+## Usage
+
+The following is an example to generate `Dockerfile` (and `cuda.repo`) that installs development components of CUDA 9.1 for CentOS 7 and cuDNN 7 on Fedora 27 Docker image.
+
+```
+$ ./generate.py --os centos7 --cuda 9.1 --cudnn 7 --variant devel --base fedora:27
+```
+
+See `./generate.py --help` for the detailed usage.
+
+## Notes
+
+* If you specify `--base` image containing different operating system than one specified in `--os`, CUDA may not work properly.
+* If you specify invalid combination (e.g., CUDA 9.1 with cuDNN 6), you may see `HTTP Error 404: Not Found` error.
+  `--verbose` option may help diagnosing such issue.
---
+++ generate.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+import os
+import sys
+
+try:
+    from urllib.request import urlopen
+except ImportError:
+    # For Python 2
+    from urllib2 import urlopen
+
+
+SYSTEMS = {
+    'ubuntu14.04': {'base': 'ubuntu:14.04'},
+    'ubuntu16.04': {'base': 'ubuntu:16.04'},
+    'ubuntu17.04': {'base': 'ubuntu:17.04'},
+    'centos6':     {'base': 'centos:6'},
+    'centos7':     {'base': 'centos:7'},
+}
+
+CUDA = ['6.5', '7.0', '7.5', '8.0', '9.0', '9.1']
+CUDNN = ['2', '3', '4', '5', '6', '7']
+
+_verbose = False
+
+
+def _log(msg):
+    if _verbose:
+        sys.stderr.write('{}\n'.format(msg))
+
+def _fetch_file(url):
+    _log('Downloading: {}'.format(url))
+    return urlopen(url).read().decode()
+
+
+def _fetch_dockerfile(url):
+    lines = []
+    for line in _fetch_file(url).splitlines():
+        if (line == 'ARG repository' or
+                line.startswith('FROM ') or
+                line.startswith('LABEL maintainer ')):
+            _log('Stripped: {}'.format(line))
+            continue
+        lines.append(line)
+    return lines
+
+
+def _generate_dockerfile(urls, os, base_image):
+    default_image = SYSTEMS[os]['base']
+    lines = [
+        '# FROM {}'.format(default_image),
+        'ARG image={}'.format(
+            default_image if base_image is None else base_image),
+        'FROM ${image}',
+    ]
+
+    for url in urls:
+        lines += [
+            '',
+            '###',
+            '### {}'.format(url),
+            '###',
+        ] + _fetch_dockerfile(url)
+    return '\n'.join(lines)
+
+
+def _generate_urls(conf):
+    stages = []
+    assets = []
+
+    # Dockerfiles.
+    base_available = conf.cuda.startswith('9.')
+    if base_available:
+        stages += ['base']
+
+    if conf.variant == 'base':
+        assert base_available
+        assert conf.cudnn == 'none'
+    elif conf.variant == 'runtime':
+        stages += ['runtime']
+    elif conf.variant == 'devel':
+        stages += ['runtime', 'devel']
+
+    if conf.cudnn != 'none':
+        stages.append('{}/cudnn{}'.format(stages[-1], conf.cudnn))
+
+    # Assets
+    if conf.os.startswith('centos'):
+        assets += ['{}/cuda.repo'.format(stages[0])]
+
+    # Translate to URL.
+    df_urls = [
+        'https://gitlab.com/nvidia/cuda/raw/{}/{}/{}/Dockerfile'.format(
+            conf.os, conf.cuda, stage)
+        for stage in stages
+    ]
+
+    asset_urls = [
+        'https://gitlab.com/nvidia/cuda/raw/{}/{}/{}'.format(
+            conf.os, conf.cuda, asset)
+        for asset in assets
+    ]
+
+    return df_urls, asset_urls
+
+
+def parse_args(args):
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '--os', type=str, required=True,
+        choices=sorted(SYSTEMS.keys()),
+        help='CUDA distribution')
+    parser.add_argument(
+        '--cuda', type=str, required=True,
+        choices=CUDA,
+        help='CUDA version')
+    parser.add_argument(
+        '--cudnn', type=str, default='none',
+        choices=(CUDNN + ['none']),
+        help='cuDNN version')
+    parser.add_argument(
+        '--variant', type=str, default='devel',
+        choices=['base', 'runtime', 'devel'],
+        help='Image variant')
+    parser.add_argument(
+        '--base', type=str, default=None,
+        help='Base Docker image')
+    parser.add_argument(
+        '--output', type=str, default='.',
+        help='Path to the output directory')
+    parser.add_argument(
+        '--verbose', action='store_true', default=False,
+        help='Log verbosely')
+
+    return parser.parse_args(args)
+
+
+def main(args):
+    global _verbose
+    conf = parse_args(args[1:])
+    _verbose = conf.verbose
+
+    df_urls, asset_urls = _generate_urls(conf)
+
+    _log('-------------------------------')
+    _log('Dockerfiles to be concatenated:')
+    for url in df_urls:
+        _log('  {}'.format(url))
+    _log('Assets to be retrieved:')
+    for url in asset_urls:
+        _log('  {}'.format(url))
+    _log('-------------------------------')
+
+    # Download resources.
+    dockerfile = _generate_dockerfile(df_urls, conf.os, conf.base)
+    assets = [_fetch_file(url) for url in asset_urls]
+
+    # Output.
+    path = '{}/Dockerfile'.format(conf.output)
+    _log('Writing: {}'.format(path))
+    with open(path, 'w') as f:
+        f.write(dockerfile)
+
+    for (url, data) in zip(asset_urls, assets):
+        path = '{}/{}'.format(conf.output, os.path.basename(url))
+        _log('Writing: {}'.format(path))
+        with open(path, 'w') as f:
+            f.write(data)
+
+    _log('Done!')
+
+
+if __name__ == '__main__':
+
+    main(sys.argv)
